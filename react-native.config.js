b0fe1aca66d97b5b49918decf460372b4b08b158
MuhammadQasimO <muhammadqasim00021@gmail.com>
2025-07-18 17:29:56 +0500

    code gen exclusion

--- react-native.config.js
+++ react-native.config.js
@@ -1,46 +1,4 @@
 module.exports = {
-  dependencies: {
-    "react-native-safe-area-context": {
-      platforms: {
-        ios: {
-          sourceDir: "../node_modules/react-native-safe-area-context/ios",
-          podfile: true,
-          scriptPhases: [
-            {
-              name: "[CP] Copy Bundle Resources",
-              script:
-                'cp -r "${PODS_ROOT}/../../node_modules/react-native-safe-area-context/ios/RCTSafeAreaContext" "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}"',
-              inputPaths: [
-                "${PODS_ROOT}/../../node_modules/react-native-safe-area-context/ios/RCTSafeAreaContext",
-              ],
-              outputPaths: [
-                "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}/RCTSafeAreaContext",
-              ],
-            },
-          ],
-        },
-      },
-    },
-    "react-native-screens": {
-      platforms: {
-        ios: {
-          sourceDir: "../node_modules/react-native-screens/ios",
-          podfile: true,
-          scriptPhases: [
-            {
-              name: "[CP] Copy Bundle Resources",
-              script:
-                'cp -r "${PODS_ROOT}/../../node_modules/react-native-screens/ios/RNScreens" "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}"',
-              inputPaths: [
-                "${PODS_ROOT}/../../node_modules/react-native-screens/ios/RNScreens",
-              ],
-              outputPaths: ["${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}/RNScreens"],
-            },
-          ],
-        },
-      },
-    },
-  },
   codegen: {
     libraries: {
       "react-native-safe-area-context": {
