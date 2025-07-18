577359415cea086c148aae2bba0465506833b784
MuhammadQasimO <muhammadqasim00021@gmail.com>
2025-07-18 18:04:23 +0500

    testing iOS

--- react-native.config.js
+++ react-native.config.js
@@ -1,18 +1,4 @@
 module.exports = {
-  codegen: {
-    libraries: {
-      "react-native-safe-area-context": {
-        ios: {
-          sourceDir: "../node_modules/react-native-safe-area-context/ios",
-          exclude: true,
-        },
-      },
-      "react-native-screens": {
-        ios: {
-          sourceDir: "../node_modules/react-native-screens/ios",
-          exclude: true,
-        },
-      },
-    },
-  },
+  // Remove the codegen exclusions that are causing crashes
+  // The new architecture needs these libraries to be properly autolinked
 };
